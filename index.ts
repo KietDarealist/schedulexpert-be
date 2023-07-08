34e7f70a26d64499d709e3b7aceb746b3c2587ef
tuankiet270802ute <kiet.huynh@usumsoftware.com>
2023-07-08 23:23:00 +0700

    fist commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript asas Server");
+});
+
+app.listen(port, () => {
+  console.log(
+    `⚡️[server]: Server is running olll at http://localhost:${port}`
+  );
+});
